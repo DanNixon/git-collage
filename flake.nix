8da76e3b340c005d6cd641448a60ead2c77c58ec
Dan Nixon <dan@dan-nixon.com>
2023-09-16 15:28:51 +0000

    Move to Nix

---
+++ flake.nix
@@ -0,0 +1,91 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, fenix, naersk }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+
+        toolchain = fenix.packages.${system}.toolchainOf {
+          channel = "1.72";
+          date = "2023-09-19";
+          sha256 = "dxE7lmCFWlq0nl/wKcmYvpP9zqQbBitAQgZ1zx9Ooik=";
+        };
+
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain.rust;
+          rustc = toolchain.rust;
+        };
+
+        cargo = builtins.fromTOML ( builtins.readFile ./Cargo.toml );
+        name = cargo.package.name;
+        version = cargo.package.version;
+
+        nativeBuildInputs = with pkgs; [ pkg-config ];
+        buildInputs = with pkgs; [ openssl ];
+
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = nativeBuildInputs ++ [ toolchain.toolchain ];
+          buildInputs = buildInputs;
+          packages = with pkgs; [ cargo-deny ];
+        };
+
+        packages = rec {
+          default = naersk'.buildPackage {
+            name = name;
+            version = version;
+
+            src = ./.;
+
+            nativeBuildInputs = nativeBuildInputs;
+            buildInputs = buildInputs;
+          };
+
+          fmt = naersk'.buildPackage {
+            src = ./.;
+
+            nativeBuildInputs = nativeBuildInputs;
+            buildInputs = buildInputs;
+
+            mode = "fmt";
+          };
+
+          clippy = naersk'.buildPackage {
+            src = ./.;
+
+            nativeBuildInputs = nativeBuildInputs;
+            buildInputs = buildInputs;
+
+            mode = "clippy";
+          };
+
+          test = naersk'.buildPackage {
+            src = ./.;
+
+            nativeBuildInputs = nativeBuildInputs;
+            buildInputs = buildInputs;
+
+            mode = "test";
+
+            # Ensure detailed test output appears in nix build log
+            cargoTestOptions = x: x ++ ["1>&2"];
+          };
+        };
+      }
+    );
+}
